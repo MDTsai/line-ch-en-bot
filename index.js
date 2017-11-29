52ceae6cfc56e9518eb7a137ef3426a67e9c1d68
MDTsai <md.tsai@gmail.com>
2017-11-29 17:33:30 +0800

    Init release to test translation

---
+++ index.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const line = require('@line/bot-sdk');
+const express = require('express');
+const translate = require('google-translate-api');
+
+// create LINE SDK config from env variables
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+};
+
+// create LINE SDK client
+const client = new line.Client(config);
+
+// create Express app
+// about Express itself: https://expressjs.com/
+const app = express();
+
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/webhook', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result));
+});
+
+// event handler
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  }
+
+  translate(event.message.text, {to: 'en'}).then(res => {
+    const translated = { type: 'text', text: res.text };
+    return client.replyMessage(event.replyToken, translated);
+  }).catch(err => {
+    console.error(err);
+  });
+}
+
+// listen on port
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
